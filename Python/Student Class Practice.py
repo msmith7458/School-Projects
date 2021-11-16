94ed050f36d783febf110c9a03451c97ab15af75
msmith7458 <89811344+msmith7458@users.noreply.github.com>
2021-11-16 13:45:50 -0500

    Update Student Class Practice.py

--- Python/Student Class Practice.py
+++ Python/Student Class Practice.py
@@ -1,4 +1,45 @@
+'''
+Student.py:
+Chapter 9, Exercise 1
+Student Name: Michael Smith
+Purpose: represent a student by a class type
+'''
+class Student(object):
+    """Represents a student"""
+    
+    def __init__(self, initName, initScore):
+        """the constructor that creates a student based on the given initial values"""
+    
+        self.name = initName
+        self.score = initScore
+    
+    def getName(self):
+        """Returns the student's name"""
+        
+        return self.name
+        
+    def setName(self, newName):
+        """Resets the student's name"""
+        
+        self.name = newName
+        
+    def getScore(self):
+        """Return's the student's score"""
+        
+        return self.score
+        
+    def setScore(self, newScore):
+        """Resets the student's score to a new score"""
+        
+        self.score = newScore
+        
+    def __str__(self):
+        """Returns a string representation of the student"""
+        
+        return "Name: " + self.name + "\nScore: " + str(self.score)
+      
 """
+main.py
 Chapter 9, Exercise 2
 Student Name: Michael Smith
 Purpose: Represent a student by a class type that is defined in a module
